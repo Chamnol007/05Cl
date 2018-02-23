d373314a145f4d2644d46be76cca5a899169c155
Chamnol_007 <chamnol_yin@cmu.ac.th>
2018-02-23 15:31:01 +0700

    add .travis.yml file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,33 @@ public class App {
         return "Hello world.";
     }
 
+    public boolean isPrime(long num){
+        double max = Math.sqrt(num) + 1;
+
+        if (num == 2){
+            return true;
+        }
+        if (num % 2 == 0){
+            return false;
+        }
+        for (long i = 3; i<max ; i += 2){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+//        System.out.println(new App().getGreeting());
+
+        if (args.length < 1 || args.length > 1){
+            System.out.println("Please input a number as the program argument");
+        }
+
+        App appObj = new App();
+        long input = Long.parseLong(args[0]);
+
+        System.out.println(appObj.isPrime(input));
     }
 }
